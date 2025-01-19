7966277b16c2577185cd1b3458c8d76c480cffdf
GGGeralt <Bartek.lipinski.pl@gmail.com>
2025-01-19 17:41:03 +0100

    finished

--- Praca Domowa 7/Assets/Scripts/StateMachine/TalkState.cs
+++ Praca Domowa 7/Assets/Scripts/StateMachine/TalkState.cs
@@ -16,19 +16,43 @@ public class TalkState : StateBase
         {
             if (stateMachine.player.transform.position == player.transform.position)
             {
-                Dictionary<FoodCenter, int> allKnownFoodCenters = stateMachine.player.foodCenterList;
+                Dictionary<FoodCenter, int> allKnownFoodCenters = null;
+
+                foreach (KeyValuePair<FoodCenter, int> item in stateMachine.player.foodCenterList)
+                {
+                    allKnownFoodCenters.Add(item.Key, item.Value);
+                }
 
                 foreach (KeyValuePair<FoodCenter, int> item in player.foodCenterList)
                 {
-                    if (!allKnownFoodCenters.ContainsKey(item.Key))
+                    if (allKnownFoodCenters.ContainsKey(item.Key) && allKnownFoodCenters[item.Key] > item.Value)
+                    {
+                        allKnownFoodCenters[item.Key] = item.Value;
+                    }
+                    else
                     {
                         allKnownFoodCenters.Add(item.Key, item.Value);
                     }
                 }
 
+                //po tych 2 foreach mamy liste wszystjkich foodCenter ktore znaja, biorac pod uwage mniejsza ilosc jedzenia jezeli oba znaja naraz
                 foreach (KeyValuePair<FoodCenter, int> item in allKnownFoodCenters)
                 {
-                    Debug.Log($"{item.Key.name}-{item.Value}");
+                    if (stateMachine.player.foodCenterList.ContainsKey(item.Key))
+                    {
+                        if (stateMachine.player.foodCenterList[item.Key] > item.Value)
+                        {
+                            stateMachine.player.foodCenterList[item.Key] = item.Value;
+                        }
+                    }
+
+                    if (player.foodCenterList.ContainsKey(item.Key))
+                    {
+                        if (stateMachine.player.foodCenterList[item.Key] > item.Value)
+                        {
+                            stateMachine.player.foodCenterList[item.Key] = item.Value;
+                        }
+                    }
                 }
             }
         }
